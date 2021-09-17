4dcdab63e0e6c38cad57938990eb36810368dca7
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2021-09-17 11:10:16 +0200

    Some work on the testing script.

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import matplotlib.pylab as plt
 import numpy as np
 import scipy.fft
 
-experiment = reader.PhillipsData.from_file_pair('Data/batch_2_clean/set_3A/n8951_5_1_raw_act.SPAR',
-                                                'Data/batch_2_clean/set_3A/n8951_5_1_raw_act.SDAT')
+experiment = reader.PhillipsData.from_file_pair('set_3A/n8951_5_1_raw_act.SPAR',
+                                                'set_3A/n8951_5_1_raw_act.SDAT')
 
 pulse_1 = experiment.data[0, :512]
 
@@ -17,25 +17,27 @@ plt.title('FID')
 plt.legend(['Real', 'Imaginary', 'Absolute'])
 plt.show()
 
-# Perform fourier transform (real)
-fft_real = scipy.fft.rfft(pulse_1.real)
-fft_imag = scipy.fft.rfft(pulse_1.imag)
-fft_abs = scipy.fft.rfft(np.absolute(pulse_1))
+# # Perform fourier transform (real)
+# fft_real = scipy.fft.rfft(pulse_1.real)
+# fft_imag = scipy.fft.rfft(pulse_1.imag)
+# fft_abs = scipy.fft.rfft(np.absolute(pulse_1))
+#
+# # # Roll fourier data
+# fft_real = np.roll(fft_real, int(len(fft_real)/2))
+# fft_imag = np.roll(fft_imag, int(len(fft_real)/2))
+# fft_abs = np.roll(fft_abs, int(len(fft_real)/2))
+#
+# plt.plot(fft_real)
+# plt.plot(np.abs(fft_real))
+#
+# plt.title('Fourier transform')
+# plt.legend(['rfft of real', 'abs rfft of real'])
+# plt.show()
 
-# # Roll fourier data
-fft_real = np.roll(fft_real, int(len(fft_real)/2))
-fft_imag = np.roll(fft_imag, int(len(fft_real)/2))
-fft_abs = np.roll(fft_abs, int(len(fft_real)/2))
-
-plt.plot(fft_real)
-plt.plot(np.abs(fft_real))
-
-plt.title('Fourier transform')
-plt.legend(['rfft of real', 'abs rfft of real'])
-plt.show()
-
+# The actual FFT as used by NMR folk.
 fft_complete = scipy.fft.fft(pulse_1)
-print(fft_complete[0])
+
+# Roll the data so the Phosphorus-  131 peak is in the center
 fft_complete = np.roll(fft_complete, int(len(fft_complete)/2))
 
 plt.plot(np.abs(fft_complete))
