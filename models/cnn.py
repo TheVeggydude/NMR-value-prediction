878ecde242ac9b1da8520d23781085b803af2837
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2022-02-08 22:34:02 +0100

    - Set the number of filters for the CNN to 1.

--- models/cnn.py
+++ models/cnn.py
@@ -7,7 +7,7 @@ def create_model(r_shape):
 
     model = keras.Sequential()
     model.add(keras.Input(shape=(261, 1000, 1)))  # Input is a whole experiment, first index is batch size.
-    model.add(layers.Conv2D(8, (7, 7, ), activation='relu'))
+    model.add(layers.Conv2D(1, (7, 7, ), activation='relu'))
     model.add(layers.AveragePooling2D((3, 3)))
     model.add(layers.Flatten())
     # model.add(layers.Dense(10, activation="relu"))
