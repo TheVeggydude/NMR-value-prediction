cedaa2dda088f724907a588da88108de3d676142
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2022-06-13 09:55:13 +0200

    Created 1D CNN models using ConstMultiplier layer.

---
+++ models/cnn_1d/v4.py
@@ -0,0 +1,38 @@
+from layers import const_multiplier
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def create_model(options):
+
+    inputs = tf.keras.Input(shape=options['input_shape'])
+
+    outputs = layers.Conv1D(
+        filters=2,
+        kernel_size=options["kernel_size"],
+        strides=1,
+        padding='same'
+    )(inputs)
+
+    outputs = layers.BatchNormalization()(outputs)
+
+    outputs = layers.Activation('sigmoid')(outputs)
+
+    outputs = const_multiplier.ConstMultiplier()(outputs)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.summary()
+
+    return model
+
+
+if __name__ == '__main__':
+
+    create_model(
+        {
+            "input_shape": (301, 512),
+            "kernel_size": 16
+        }
+    )
