079fbed1d81ce2d4ae96b51f8ccb50a7f5f84f78
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2022-08-29 16:26:53 +0200

    Cleaned up plotting single and multiple scripts.

--- plotting_multi.py
+++ plotting_multi.py
@@ -1,90 +1,87 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+import scipy.stats
 import util
 
 setups = [
-    # {
-    #     "name": "1d_cnn_v1_raw",
-    #     "dataset": "simple_simulation_raw",
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
     {
-        "name": "1d_cnn_v1_proc",
-        "dataset": "simple_simulation_proc",
+        "name": "2d_cnn_v1",
+        "display": "1 layer",
+        "dataset": "simple_sim_norm",
 
-        "batch_size": 32,
-        "epochs": 10000,
         "runs": 10
     },
-    # {
-    #     "name": "1d_cnn_v4_k8",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
     {
-        "name": "1d_cnn_v4",
-        "dataset": "simple_simulation_proc",
+        "name": "2d_cnn_v4",
+        "display": "2 layers",
+        "dataset": "simple_sim_norm",
 
-        "batch_size": 32,
-        "epochs": 10000,
         "runs": 10
     },
-    # {
-    #     "name": "1d_cnn_v4_k32",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v4_k64",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
     {
-        "name": "1d_cnn_v7",
-        "dataset": "simple_simulation_proc",
+        "name": "2d_cnn_v3",
+        "display": "3 layers",
+        "dataset": "simple_sim_norm",
 
-        "batch_size": 32,
-        "epochs": 10000,
+        "runs": 10
+    },
+    {
+        "name": "2d_cnn_v8",
+        "display": "4 layers",
+        "dataset": "simple_sim_norm",
+
         "runs": 10
     },
 ]
 
 if __name__ == '__main__':
-    all_predictions_for_setup = []
+    all_loss_for_setup = []
+    params_per_setup = [None for x in range(len(setups))]
 
-    for setup in setups:
-        prediction_results = []
+    for index, setup in enumerate(setups):
+        loss = []
 
         for run in range(setup['runs']):
-            data_test, ground_test, model, eval_score, predictions, _ = util.load_batch_setup_and_eval(setup, run)
+            # if run == 3:
+            #     continue
 
-            mse_results = util.compute_mse(predictions, ground_test)
-            prediction_results.extend([x[0] for x in mse_results])
+            data_test, ground_test, predictions, _, params = util.load_batch_setup_and_eval(setup, run)
+            params_per_setup[index] = params
 
-        prediction_results = np.asarray(prediction_results)
+            loss_index_pair = util.compute_mse(predictions, ground_test)
+            loss.extend([x[0] for x in loss_index_pair])
+
+        loss = np.asarray(loss)
 
         # Scale MSE values to the same scale for comparison
         # prediction_results = prediction_results / (np.amax(prediction_results))
 
-        all_predictions_for_setup.append(prediction_results)
+        plt.scatter(params_per_setup[index], np.mean(loss))
 
-    plt.hist(all_predictions_for_setup)
+        all_loss_for_setup.append(loss)
+
+    # Display neurons vs loss
+    plt.title("Mean Loss VS Neuron Count")
+    plt.legend([setup["name"] for setup in setups])
+    plt.show()
+
+    counts, edges, bars = plt.hist(all_loss_for_setup)
 
     plt.title("Prediction error distribution per setup")
-    plt.legend([setup["name"] for setup in setups])
+    plt.legend([setup["display"] for setup in setups])
+    # plt.bar_label(bars)
     plt.ylabel("Count")
     plt.xlabel("Mean Squared Error")
     plt.show()
+
+    info = [
+        (setups[i]['name'], np.mean(x), np.std(x), scipy.stats.ttest_rel(x, all_loss_for_setup[i + 1 if i < len(setups) - 1 else 0]))
+        for i, x in enumerate(all_loss_for_setup)
+    ]
+    # info = [
+    #     (setups[i]['name'], np.mean(x), np.std(x), scipy.stats.ttest_rel(x, all_loss_for_setup[-2]))
+    #     for i, x in enumerate(all_loss_for_setup)
+    # ]
+
+    print(info)
--- plotting_single.py
+++ plotting_single.py
@@ -1,70 +1,92 @@
 import plotting.plotting as plot
+from operator import itemgetter
 
 import util
 
 
 setup = {
-        "name": "2d_cnn_v3_f_64_32",
-        "dataset": "simple_simulation_proc",
-
-        "batch_size": 32,
-        "epochs": 10000,
-        "runs": 10
+        "name": "2d_cnn_v1",
+        "display": "1 layer",
+        "dataset": "simple_sim_norm",
     }
+
 
 # Setup to compare fits with
 comp_setup = {
-        "name": "2d_cnn_v3_f_16_8",
-        "dataset": "simple_simulation_proc",
-
-        "batch_size": 32,
-        "epochs": 10000,
-        "runs": 10
+        "name": "2d_cnn_v3",
+        "display": "3 layer",
+        "dataset": "simple_sim_norm",
     }
 
 test_batch = 0
+param = 1
 
 
 if __name__ == '__main__':
 
-    data_test, ground_test, model, eval_score, predictions, noises = util.load_batch_setup_and_eval(setup, test_batch)
-    _, _, comp_model, comp_eval_score, comp_predictions, _ = util.load_batch_setup_and_eval(comp_setup, test_batch)
+    data_test, ground_test, predictions, sim_params, _ = util.load_batch_setup_and_eval(setup, test_batch)
+    _, _, comp_predictions, _, _ = util.load_batch_setup_and_eval(comp_setup, test_batch)
 
     # Valuate results
-    mse_results = util.compute_mse(predictions, ground_test, noises)
-    sorted_mse_results = sorted(mse_results)
+    mse_results = util.compute_mse(predictions, ground_test)
+    mse_results = [(x[0], x[1], sim_params[x[1], param]) for x in mse_results]
+    mse_results.sort(key=itemgetter(0))
 
     # Plot best and worst fits
-    plot.plot_pcr_subset(sorted_mse_results[:10], ground_test, setup['name'] + " - Best fits")
-    plot.plot_pcr_subset(sorted_mse_results[-10:], ground_test, setup['name'] + " - Worst fits")
+    plot.plot_pcr_subset(mse_results[:10], ground_test, setup['display'] + " - Best fits")
+    plot.plot_pcr_subset(mse_results[-10:], ground_test, setup['display'] + " - Worst fits")
 
     # Plot best fit
-    best_idx = sorted_mse_results[0][1]
-    noise = util.compute_noise_metric(data_test[best_idx])
+    score, idx, _ = mse_results[0]
+    # plot.plot_input(data_test[idx], "Best")
 
-    plot.plot_prediction_and_ground(
-        predictions[best_idx],
-        ground_test[best_idx],
-        "Best prediction (noise = " + str(noise) + ")",
-        setup['name'],
-        comp_predictions[best_idx],
-        comp_setup['name']
+    plot.plot_ground_pcr_pii(
+        predictions[idx],
+        ground_test[idx],
+        # "Best (" + str(score) + ", " + str(sim_params[idx][0]) + ")",
+        "Best (" + str(score) + ")",
+        setup['display'],
+        comp_predictions[idx],
+        comp_setup['display']
     )
 
     # Plot worst fit
-    worst_idx = sorted_mse_results[-1][1]
-    noise = util.compute_noise_metric(data_test[worst_idx])
+    score, idx, _ = mse_results[-1]
+    # plot.plot_input(data_test[idx], "Worst")
 
-    plot.plot_prediction_and_ground(
-        predictions[worst_idx],
-        ground_test[worst_idx],
-        "Worst prediction (noise = " + str(noise) + ")",
-        setup['name'],
-        comp_predictions[worst_idx],
-        comp_setup['name']
+    plot.plot_ground_pcr_pii(
+        predictions[idx],
+        ground_test[idx],
+        # "Worst (" + str(score) + ", " + str(sim_params[idx][0]) + ")",
+        "Worst (" + str(score) + ")",
+        setup['display'],
+        comp_predictions[idx],
+        comp_setup['display']
     )
 
-    noise_levels = [util.compute_noise_metric(dp) for dp in data_test]
-    mse_scores = [result[0] for result in mse_results]
-
-    plot.plot_mse_vs_noise(mse_scores, noise_levels)
+    # mse_results.sort(key=itemgetter(2))
+    #
+    # score, idx, _ = mse_results[0]
+    # plot.plot_input(data_test[idx], str(sim_params[idx][0:3]))
+    #
+    # plot.plot_prediction_and_ground(
+    #     predictions[idx],
+    #     ground_test[idx],
+    #     "Best (" + str(score) + ", " + str(sim_params[idx][0]) + ")",
+    #     setup['name'],
+    #     # comp_predictions[idx],
+    #     # comp_setup['name']
+    # )
+    #
+    # # Plot worst fit
+    # score, idx, _ = mse_results[-1]
+    # plot.plot_input(data_test[idx], str(sim_params[idx][0:3]))
+    #
+    # plot.plot_prediction_and_ground(
+    #     predictions[idx],
+    #     ground_test[idx],
+    #     "Worst (" + str(score) + ", " + str(sim_params[idx][0]) + ")",
+    #     setup['name'],
+    #     # comp_predictions[idx],
+    #     # comp_setup['name']
+    # )
