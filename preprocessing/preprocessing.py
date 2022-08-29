c23d73676c016fa561d27144ff3d8eb68ba2f260
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2022-08-29 15:34:23 +0200

    Updated preprocessing to normalize for individual experiments.

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -11,11 +11,11 @@ def as_absolute(x):
 
 def preprocess(data, ground):
 
-    # Get max value of data
-    norm_scalar = np.amax(data)
-
     # For each experiment in the dataset
     for i, experiment in enumerate(data):
+
+        # Get max value of data
+        norm_scalar = np.amax(experiment)
 
         data[i] = np.asarray([experiment / norm_scalar])  # scale data
         ground[i] = np.asarray([ground[i] / norm_scalar])  # scale ground truth
