850e72fcc34ad8ea763dfa1a949d18adb6179a33
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2022-08-29 16:25:36 +0200

    Added learning rate to setup description

--- training.py
+++ training.py
@@ -1,274 +1,36 @@
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
 import numpy as np
 import tensorflow as tf
 
 import models
 
 setups = [
-    # {
-    #     "name": "2d_cnn_simple_1filter",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn",
-    #     "v": 8,
-    #     "dimensions": ((301, 2), (301, 512, 1)),
-    #     "n_rep": 1,
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "2d_cnn_simple_8filters",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn",
-    #     "v": 9,
-    #     "dimensions": ((301, 2), (301, 512, 1)),
-    #     "n_rep": 1,
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "2d_cnn_simple_32filters",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn",
-    #     "v": 11,
-    #     "dimensions": ((301, 2), (301, 512, 1)),
-    #     "n_rep": 1,
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_simple_kernel4",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn",
-    #     "v": 3,
-    #     "dimensions": ((301, 2), (301, 512)),
-    #     "n_rep": 1,
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_simple_kernel8",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn",
-    #     "v": 4,
-    #     "dimensions": ((301, 2), (301, 512)),
-    #     "n_rep": 1,
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_simple_kernel16",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn",
-    #     "v": 5,
-    #     "dimensions": ((301, 2), (301, 512)),
-    #     "n_rep": 1,
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_simple_kernel32",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn",
-    #     "v": 6,
-    #     "dimensions": ((301, 2), (301, 512)),
-    #     "n_rep": 1,
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_simple_kernel16",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn_api",
-    #     "dimensions": ((301, 2), (301, 512)),
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_batch_kernel16",
-    #     "dataset": "2022-02-19T16_52_00",
-    #     "type": "cnn_api_batch",
-    #     "dimensions": ((301, 2), (301, 512)),
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v1_raw",
-    #     "dataset": "simple_simulation_raw",
-    #
-    #     "model": models.cnn_1d.v1.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 16
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v1_proc",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v1.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 16
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v2",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v2.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 16
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v3",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v3.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 16
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v4",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v4.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 16
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v7",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v7.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 16
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v4_k32",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v4.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 32
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v4_k64",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v4.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 64
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v5",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v5.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 16
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
-    # {
-    #     "name": "1d_cnn_v6",
-    #     "dataset": "simple_simulation_proc",
-    #
-    #     "model": models.cnn_1d.v6.create_model,
-    #     "options": {
-    #         "input_shape": (301, 512),
-    #         "kernel_size": 32
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
     {
-        "name": "2d_cnn_v3",
-        "dataset": "simple_simulation_proc",
+        "name": "2d_cnn_v1",
+        "dataset": "simple_sim_norm",
 
-        "model": models.cnn_2d.v3.create_model,
+        "model": models.cnn_2d.v1.create_model,
         "options": {
-            # 'filters': 1,
             'input_shape': (301, 512, 1),
             'output_shape': (301, 2, 1),
+            'learning_rate': 0.001,
+            'filters': [
+                32,
+                16
+            ],
+            'kernels': [
+                (10, 24),
+                (10, 10),
+                (10, 2),
+            ]
         },
 
-        "batch_size": 32,
+        "batch_size": 8,
         "epochs": 10000,
         "runs": 10
     },
-    # {
-    #     "name": "2d_cnn_v4_cropped",
-    #     "dataset": "simple_simulation_proc_cropped",
-    #
-    #     "model": models.cnn_2d.v4.create_model,
-    #     "options": {
-    #         'filters': 2,
-    #         'input_shape': (301, 55, 1),
-    #         'output_shape': (301, 2, 1),
-    #     },
-    #
-    #     "batch_size": 32,
-    #     "epochs": 10000,
-    #     "runs": 10
-    # },
 ]
 
 K = 10
@@ -320,14 +82,19 @@ if __name__ == '__main__':
 
             model = setup["model"](setup['options'])
 
+            optimizer = tf.keras.optimizers.Adam(
+                learning_rate=setup["options"]["learning_rate"]
+            )
+
             model.compile(
                 loss="mean_squared_error",
-                optimizer='adam',
+                optimizer=optimizer,
                 metrics=[
                     tf.keras.metrics.RootMeanSquaredError(),
                     tf.keras.metrics.MeanAbsoluteError(),
                     tf.keras.metrics.MeanAbsolutePercentageError()
-                ]
+                ],
+                # jit_compile=True
             )
 
             ###
